dbe1d1cf1e0b65744f9fe71f9338163b6ec0f9cb
di202313 <144887714+di202313@users.noreply.github.com>
2024-02-06 11:14:35 +0500

    Update 12.cpp

--- 12/12.cpp
+++ 12/12.cpp
@@ -1,18 +1,3 @@
-﻿#include <iostream>
-using namespace std;
-
-int cardNumber = 1234567890;
-int pin = 1234;
-
-float balance = 40000;
-
-bool checkAmount(double amount) {
-    if (amount < 5.0 || amount != (int)amount) {
-        cout << "Введена неверная сумма" << endl;
-        return false;
-    }
-    return true;
-}
 
 //Функции сюда нужно вставить
 
