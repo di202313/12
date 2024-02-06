138b5a19a24df6cc9f28d6a3d1ede4f84899a4ab
1-РПО-23 <1--23@DESKTOP-4NL7940>
2024-02-06 10:51:50 +0500

    Elvira

--- 12/12.cpp
+++ 12/12.cpp
@@ -1,77 +1,54 @@
 ﻿#include <iostream>
 using namespace std;
 
-class Bankomat {
+double withdrawMoney(double balance) {
+    double amount;
+    cout << "Введите сумму для снятия: ";
+    cin >> amount;
+    if (checkAmount(amount) && balance >= amount) {
+        balance -= amount;
+        cout << "Операция выполнена успешно" << endl;
+    }
+    else {
+        cout << "Недостаточно средств или введена неверная сумма" << endl;
+    }
+    return balance;
+}
 
-private:
-    long long cardNumber;
-    int pinCode;
-    int balance;
+double depositMoney(double balance) {
+    double amount;
+    cout << "Введите сумму для пополнения: ";
+    cin >> amount;
+    if (checkAmount(amount)) {
+        balance += amount;
+        cout << "Операция выполнена успешно" << endl;
+    }
+    else {
+        cout << "Введена неверная сумма" << endl;
+    }
+    return balance;
+}
 
-public:
-    Bankomat(long long card, int pin, int bal) : cardNumber(card), pinCode(pin), balance(bal) {}
-    
-    void login() {
-        setlocale(0, "");
-        long long inputCard;
-        int inputPin;
-
-        for (int i = 0; i < 3; i++) {
-            cout << "Введите номер карты: ";
-            cin >> inputCard;
-            cout << "Введите pin-код: ";
-            cin >> inputPin;
-
-            if (inputCard == cardNumber && inputPin == pinCode) {
-                cout << "Вход в систему успешен.\n";
-                return;
-            }
-            else {
-                cout << "Неверный номер карты или pin-код. Пожалуйста, попробуйте снова.\n";
-            }
+void payPhone(float balance, char operatorCode) {
+    float amount, commission;
+    cout << "Введите сумму для оплаты телефона: ";
+    cin >> amount;
+    if (checkAmount(amount) && balance >= amount) {
+        if (operatorCode == 'B') {
+            commission = amount * 0.05;
         }
-
-        cout << "Ошибка входа. Доступ по этой карте запрещен\n";
-    }
-
-    void checkBalance() {
-        setlocale(0, "");
-        cout << "Ваш баланс равен: " << balance << "р\n";
-    }
-
-    void withdrawCash(int amount) {
-        setlocale(0, "");
-        if (amount % 5 != 0 || amount > balance) {
-            cout << "Неверная сумма или недостаточно средств.\n";
+        else if (operatorCode == 'M') {
+            commission = amount * 0.07;
         }
         else {
-            balance -= amount;
-            cout << "Успешно выведен: " << amount << "р\n";
+            cout << "Некорректный код оператора\n";
+            return;
         }
+        float totalPayment = amount + commission;
+        balance -= totalPayment;
+        cout << "Оплачено " << amount << " рублей. Комиссия: " << commission << " рублей. Остаток на счете: " << balance << " рублей\n";
     }
-
-    void depositCash(int amount) {
-        setlocale(0, "");
-        if (amount % 5 != 0) {
-            cout << "Недопустимая сумма. Вносите депозит только в количестве, кратном 5р.\n";
-        }
-        else {
-            balance += amount;
-            cout << "Успешно депонирован: " << amount << "р\n";
-        }
+    else {
+        cout << "Недостаточно средств или введена неверная сумма" << endl;
     }
-};
-
-int main() {
-    Bankomat user1(123456789, 1234, 30000);
-
-    user1.login();
-
-    user1.checkBalance();
-
-    user1.withdrawCash(10000);
-
-    user1.depositCash(6000);
-
-    return 0;
 }
